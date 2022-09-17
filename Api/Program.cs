3b5a243d80d412786d2be8ae1cc60a65574b4099
amirsolhi <a.solhi@alibaba.ir>
2022-09-17 23:54:22 +0430

    grpc transcoding swagger enabled

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.OpenApi.Models;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo { Title = "gRPC transcoding", Version = "v1" });
+});
 builder.Services.AddGrpcReflection();
 
 builder.WebHost.ConfigureKestrel(kestrel =>
@@ -13,7 +20,14 @@ builder.WebHost.ConfigureKestrel(kestrel =>
 var app = builder.Build();
 
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+
 app.MapGrpcService<ProductAppService>();
 app.MapGrpcReflectionService();
+app.MapGet("/", () => "Grpc trasncoding is enabled. Use Postman please");
 
 await app.RunAsync();
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 rm Contracts/products.pb || true
 protoc -I Contracts -I. --include_imports --include_source_info  --descriptor_set_out=Contracts/products.pb Contracts/products.proto
-docker-compose up
+docker-compose up --build
